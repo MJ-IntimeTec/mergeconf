d9c4302411e7217071478d1627d0c18752c0e5e6
MJ-Itt <mihir.jain@intimetec.com>
2024-01-22 13:45:53 +0530

    chANGES

---
+++ pal.ps1
@@ -0,0 +1,23 @@
+﻿
+function palimdromeone{
+    param(
+        [int]$number
+    )
+    $rev = 0
+    $n = $num
+	while($num -ne 0){
+		$rem = $num % 10
+		$rev = ($rev*10)+$rem
+		$num = [math]::Floor($num/10)
+	} 
+    Write-Host "$n"
+    Write-Host "$rev"
+	 if ($n -eq $rev) {
+        Write-Host "The number is a palindrome .$n"
+    } else {
+        Write-Host "The number is not a palindrome.$n"
+    }
+
+}
+
+
